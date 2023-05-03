6c538063039181b6f17b842d2609453a1ee226a7
Kevin Hwang <nait90@gmail.com>
2023-05-03 14:56:49 +0900

    rename to hello

---
+++ .history/1_echo/echo_20230503143546.js
@@ -0,0 +1,19 @@
+const os = require('os')
+const http = require('http')
+
+const port = 8000
+let cnt = 0
+
+http.createServer((req, res) => {
+  /// don't increment the counter if the favicon.ico is being requested
+  if (req.url.toLowerCase() === '/favicon.ico') {
+    res.writeHead(200, {'Content-Type': 'image/x-icon'})
+    res.end()
+    console.log('favicon requested')
+    return
+  }
+
+  res.end(`Node Bonkour on ${os.hostname()} ${cnt++} \n`)
+}).listen(port)
+
+console.log(`Server running at http://localhost:${port}`)
